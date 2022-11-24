4d9dfef66f72462972df03bdd08966738fae04ce
Trevoir Williams <trevoir.williams@gmail.com>
2022-11-24 11:07:14 -0500

    Manipulated Website Views and Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
