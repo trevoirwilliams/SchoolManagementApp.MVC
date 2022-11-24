800a3308dd49a923f033bf715c2625cfed4ece32
Trevoir Williams <trevoir.williams@gmail.com>
2022-11-24 09:43:19 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolManagementApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
